4c891b3088377baf89c5b50be4c29bcde8f22d5b
sharewind <sharewind@gmail.com>
2014-05-02 20:00:50 +0800

    set timezone

--- setup.sh
+++ setup.sh
@@ -3,4 +3,9 @@ set -x
 
 ln -sf ~/projects/dotfiles-ubuntu/.zshrc ~/.zshrc
 
+#set timezone
+sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
+sudo ntpdate cn.pool.ntp.org
+
+
 #ssh-keygen 
